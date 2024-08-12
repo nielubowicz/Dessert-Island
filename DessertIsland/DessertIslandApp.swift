bb4c8976bbccb7d3474afd6c01825952e81bc662
Chris Nielubowicz <nielubowicz@gmail.com>
2024-08-12 13:26:04 -0400

    Initial Commit

---
+++ DessertIsland/DessertIslandApp.swift
@@ -0,0 +1,17 @@
+//
+//  DessertIslandApp.swift
+//  DessertIsland
+//
+//  Created by mac on 8/12/24.
+//
+
+import SwiftUI
+
+@main
+struct DessertIslandApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
