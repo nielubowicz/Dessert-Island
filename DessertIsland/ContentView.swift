317ba8c8158050c54ac555485fa69f3a632b312b
Chris Nielubowicz <nielubowicz@gmail.com>
2024-08-18 09:36:32 -0400

    Use Result<> type for return values.
    Add progressView for loading, error screen for error handling.

--- DessertIsland/ContentView.swift
+++ DessertIsland/ContentView.swift
@@ -11,26 +11,51 @@ import SwiftUI
  The user should be shown the list of meals in the Dessert category, sorted alphabetically.
  */
 struct ContentView: View {
-    @State var mealList: MealList = MealList(meals: [Meal]() )
+    @State var mealList: Result<MealList, Error>?
     
     var body: some View {
         NavigationView {
-            List {
-                ForEach(mealList.meals) { meal in
-                    RowView(meal: meal)
+            VStack {
+                switch mealList {
+                case .success(let mealList):
+                    List {
+                        ForEach(mealList.meals) { meal in
+                            RowView(meal: meal)
+                        }
+                    }
+                    
+                case .failure(let error):
+                    VStack(alignment: .center) {
+                        Text(error.localizedDescription)
+                    }
+                    
+                case nil:
+                    ProgressView()
+                        .frame(maxWidth: .infinity, maxHeight: .infinity)
+                        .background {
+                            Color.secondary.opacity(0.6)
+                        }
+                        .animation(.easeInOut(duration: 2.0), value: mealList != nil)
+                    
                 }
-            }.navigationTitle("Dessert Island")
-        }.task {
+            }
+            .navigationTitle("Dessert Island")
+        }
+        .task {
             do {
                 mealList = try await NetworkCall.shared.getMealsOfType("Dessert")
             } catch {
-                // TODO: Handle errors here
+                // TODO: Handle error
+                // TODO: Add an alert with a retry button
             }
         }
     }
 }
 
 #Preview {
-    ContentView()
+    ContentView(mealList: .success(MealList(meals: [Meal]() )))
 }
 
+#Preview {
+    ContentView(mealList: nil)
+}
--- DessertIsland/NetworkCalls.swift
+++ DessertIsland/NetworkCalls.swift
@@ -24,16 +24,18 @@ class NetworkCall: ObservableObject {
         }
     }
     
-    func getMealsOfType(_ category: String) async throws -> MealList {
-        guard let url = URL(for: .list(category: category)) else { throw NetworkError.badURL }
+    func getMealsOfType(_ category: String) async throws -> Result<MealList, Error> {
+        guard let url = URL(for: .list(category: category)) else { return .failure(NetworkError.badURL) }
         let URLRequest = URLRequest(url: url)
         
+        // Make request
         let (data, response) = try await URLSession.shared.data(for: URLRequest)
-        guard let response = response as? HTTPURLResponse else { throw NetworkError.noResponse }
         
-        // TODO: genericize this call to handle more types of status codes
-        guard response.statusCode == 200 else { throw NetworkError.status(code: response.statusCode) }
-        return try JSONDecoder().decode(MealList.self, from: data)
+        // Validate response
+        guard let response = response as? HTTPURLResponse else { return .failure(NetworkError.noResponse) }
+        guard response.statusCode == 200 else { throw NetworkError.status(code: response.statusCode) } // TODO: genericize this call to handle more types of status codes
+        
+        return .success(try JSONDecoder().decode(MealList.self, from: data))
     }
     
     func getMealDetails(for mealID: String) async throws -> MealLookup {
